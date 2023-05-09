2dae75d00663058f3788c345fe3b1005c9788fae
WalimatulUrs <88244502+WalimatulUrs@users.noreply.github.com>
2023-05-09 08:47:10 +0800

    Initial commit

---
+++ src/components/WishesSection/wishlist-data.js
@@ -0,0 +1,10 @@
+import Image from '@assets/images/couple-2.jpg';
+
+export const wishlist = [
+  {
+    name: 'Kamal',
+    infoName: 'Sahabat Mempelai',
+    description: `Congrats Fulan & Fulanah for your marriage. You are a perfect pair. You are an inspiration to me. And have a blissful conjugal life!`,
+    image: Image,
+  },
+];
